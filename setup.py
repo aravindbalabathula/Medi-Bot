3fee78757fd3855988ca41acb4b1ec66dbdecd74
aravindbalabathula <aravindbalabathula99@gmail.com>
2025-10-21 23:16:21 +0530

    Initial updation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Balabathula Aravind",
+    author_email="aravindbalabathula99@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
